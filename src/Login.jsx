28f5a48a1e842a295e6e048f629b9afe174f0aef
muhammadumersaleem408@gmail.com <66533954+Muhammad506@users.noreply.github.com>
2024-09-04 22:34:59 +0500

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,41 +1,43 @@
 import { MdEmail } from "react-icons/md";
 import { RiLock2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <main className="max-h-screen flex flex-col md:flex-row text-white md:overflow-y-visible lg:overflow-y-hidden">
-      <div className="w-full md:w-2/3 opacity-10 md:opacity-100 -mt-40">
-        <a
-          className="absolute z-50 left-2 top-2 opacity-75 hover:opacity-100 text-white border rounded-3xl px-4 py-1"
-          href="/"
-        >
-          Back
-        </a>
+    <main className="h-screen flex flex-col md:flex-row text-white overflow-visible md:overflow-hidden">
+      <a
+        className="absolute z-50 left-4 top-4 opacity-90 hover:opacity-100 text-white border border-white rounded-3xl px-4 py-1 transition duration-300 ease-in-out bg-black bg-opacity-50 shadow-lg"
+        href="/"
+      >
+        Back
+      </a>
+      <div className="w-full md:w-2/3 relative hidden md:block">
         <img
-          src="https://intelliwriter.io/backgrounds/Loginbg.webp"
+          src="LR-bg.jpg"
           alt="background"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover opacity-50 md:opacity-100"
         />
       </div>
 
-      <div className="bg-[#0F172A] w-full md:w-1/3 p-5 flex flex-col items-center py-6">
-        <img src="navbar.png" alt="navbar" className="px-28 pb-10" />
-        <h1 className="font-bold text-4xl text-center">Login</h1>
-        <p className="text-gray-300 md:py-2 py-5 lg:py-5 px-6 lg:px-14 text-center">
-          Start generating your thoughts with Intelliwriter.io
+      <div className="bg-[#0F172A] w-full md:w-2/5 flex flex-col items-center p-6 md:p-6 justify-center md:relative md:top-0 shadow-xl ">
+        <img src="NavBar.png" alt="navbar" className="w-24 mb-4" />
+        <h1 className="font-extrabold text-4xl text-center mb-3 text-yellow-400">Login</h1>
+        <p className="text-gray-300 mb-3 px-4 text-center text-lg">
+          Start generating your thoughts with Solar Intelli Solutions.
         </p>
-        <form className="w-full max-w-xs">
-          <div className="mb-6">
+        <form className="w-full max-w-xs space-y-4">
+          {/* Email */}
+          <div>
             <label
-              className="block text-gray-300 text-sm font-bold mb-2"
+              className="block text-gray-300 text-sm font-semibold mb-1"
               htmlFor="email"
             >
               Email
             </label>
-            <div className="flex items-center border rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-2">
-              <MdEmail className="text-white mr-2" />
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-3 shadow-md">
+              <MdEmail className="text-yellow-400 mr-2" />
               <input
-                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
+                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 focus:outline-none placeholder-gray-400"
                 type="email"
                 id="email"
                 placeholder="Enter your email"
@@ -43,17 +45,19 @@ const Login = () => {
               />
             </div>
           </div>
-          <div className="mb-6">
+
+          {/* Password */}
+          <div>
             <label
-              className="block text-gray-300 text-sm font-bold mb-2"
+              className="block text-gray-300 text-sm font-semibold mb-1"
               htmlFor="password"
             >
               Password
             </label>
-            <div className="flex items-center border rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-2">
-              <RiLock2Fill className="text-white mr-2" />
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-3 shadow-md">
+              <RiLock2Fill className="text-yellow-400 mr-2" />
               <input
-                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
+                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 focus:outline-none placeholder-gray-400"
                 type="password"
                 id="password"
                 placeholder="Enter your password"
@@ -61,24 +65,25 @@ const Login = () => {
               />
             </div>
           </div>
-          <div className="flex items-center justify-between">
+
+          <div className="flex items-center justify-between mt-4">
             <a
               href="#"
-              className="inline-block align-baseline font-bold text-sm text-pink-500 hover:text-pink-700"
+              className="inline-block font-bold text-sm text-yellow-400 hover:text-yellow-500 transition duration-200"
             >
               Forgot password?
             </a>
           </div>
-          <div className="mt-8 mb-6">
-            <button className="bg-gradient-to-r from-[#6F26C8] to-[#4B1D84] hover:from-[#4B1D84] hover:to-[#6F26C8] text-white font-bold py-2 px-4 rounded-full w-full">
+          <div className="mt-4">
+            <button className="bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-600 hover:to-yellow-400 text-black font-bold py-2 px-4 rounded-full w-full shadow-lg transition-transform transform hover:scale-105 duration-300">
               Login
             </button>
           </div>
           <p className="mt-4 text-center">
-            Don't have an account?
-            <a href="#" className="text-pink-500 hover:text-pink-700">
+            Don't have an account?{" "}
+            <Link to="/register" className=" font-semibold text-yellow-400 hover:text-yellow-500 transition duration-200">
               Register
-            </a>
+            </Link>
           </p>
         </form>
       </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,40 +1,48 @@
 import { CgProfile } from "react-icons/cg";
 import { MdEmail } from "react-icons/md";
 import { RiLock2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <main className="h-screen flex flex-col md:flex-row text-white overflow-hidden">
-      <div className="w-full md:w-2/3 relative opacity-10 md:opacity-100 object-cover">
-        <a
-          className="absolute z-50 left-2 top-2 opacity-75 hover:opacity-100 text-white border rounded-3xl px-4 py-1"
-          href="/"
-        >
-          Back
-        </a>
+    <main className="h-screen flex flex-col md:flex-row text-white overflow-visible md:overflow-hidden">
+      <a
+        className="absolute z-50 left-4 top-4 opacity-90 hover:opacity-100 text-white border border-white rounded-3xl px-4 py-1 transition duration-300 ease-in-out"
+        href="/"
+      >
+        Back
+      </a>
+      <div className="w-full md:w-2/3 relative hidden md:block">
         <img
-          src="https://intelliwriter.io/backgrounds/Loginbg.webp"
+          src="LR-bg.jpg"
           alt="background"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover opacity-30 md:opacity-100"
         />
       </div>
 
-      <div className="bg-[#0F172A] w-full md:w-1/3 p-5 flex flex-col items-center overflow-y-auto">
-        <img src="navbar.png" alt="navbar" className="px-28 pb-10" />
-        <h1 className="font-bold text-4xl text-center">Register</h1>
-        <p className="text-gray-300 py-5 px-6 text-center">
-          Start generating your thoughts with Intelliwriter.io
+      <div className="bg-[#0F172A] w-full md:w-2/5 flex flex-col items-center p-4 md:p-2 justify-center md:relative md:top-0">
+        <img src="NavBar.png" alt="navbar" className="w-20 " />
+        <h1 className="font-extrabold text-3xl text-center mb-2 text-yellow-400">Register</h1>
+        <p className="text-gray-400 mb-2 px-4 text-center">
+          Start generating your thoughts with Solar Intelli Solutions.
         </p>
 
-        <form className="w-full max-w-xs space-y-4">
+        <form className="w-full max-w-xs space-y-2">
           {/* Name */}
           <div>
-            <div className="flex items-center border rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] p-2">
+            <label
+              className="block text-gray-300 text-sm font-semibold mb-1"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-2">
               <CgProfile className="text-white mr-2" />
               <input
-                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
+                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 focus:outline-none placeholder-gray-400"
                 type="text"
-                placeholder="Name"
+                id="name"
+                placeholder="Enter your name"
                 aria-label="Name"
               />
             </div>
@@ -42,12 +50,19 @@ const Register = () => {
 
           {/* Email */}
           <div>
-            <div className="flex items-center border rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] p-2">
+            <label
+              className="block text-gray-300 text-sm font-semibold mb-1"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-2">
               <MdEmail className="text-white mr-2" />
               <input
-                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
+                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 focus:outline-none placeholder-gray-400"
                 type="email"
-                placeholder="Email"
+                id="email"
+                placeholder="Enter your email"
                 aria-label="Email"
               />
             </div>
@@ -55,12 +70,19 @@ const Register = () => {
 
           {/* Password */}
           <div>
-            <div className="flex items-center border rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] p-2">
+            <label
+              className="block text-gray-300 text-sm font-semibold mb-1"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-2">
               <RiLock2Fill className="text-white mr-2" />
               <input
-                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
+                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 focus:outline-none placeholder-gray-400"
                 type="password"
-                placeholder="Password"
+                id="password"
+                placeholder="Enter your password"
                 aria-label="Password"
               />
             </div>
@@ -69,33 +91,35 @@ const Register = () => {
           {/* Confirm Password */}
           <div>
             <label
-              className="block text-gray-300 text-sm font-bold mb-2"
+              className="block text-gray-300 text-sm font-semibold mb-1"
               htmlFor="confirmation"
             >
-              Strong Password: IntelliWriter@738
+              Confirm Password
             </label>
-            <div className="flex items-center border rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] p-2">
+            <div className="flex items-center border border-gray-600 rounded-lg bg-gradient-to-t from-[#1C204B] to-[#10172D] py-2 px-2">
               <RiLock2Fill className="text-white mr-2" />
               <input
-                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 leading-tight focus:outline-none"
+                className="appearance-none bg-transparent border-none w-full text-white py-1 px-2 focus:outline-none placeholder-gray-400"
                 type="password"
-                placeholder="Confirm Your Password"
+                id="confirmation"
+                placeholder="Confirm your password"
                 aria-label="Confirmation"
               />
             </div>
           </div>
 
-          <button className="bg-gradient-to-r from-[#6F26C8] to-[#4B1D84] hover:from-[#4B1D84] hover:to-[#6F26C8] text-white font-bold py-2 px-4 rounded-full w-full">
-            Register
-          </button>
+          <div className="mt-4">
+            <button className="bg-gradient-to-r from-yellow-400 to-yellow-600 hover:from-yellow-600 hover:to-yellow-400 text-black font-bold py-2 px-4 rounded-full w-full shadow-lg transition-transform transform hover:scale-105 duration-300">
+              Register
+            </button>
+          </div>
+          <p className="mt-4 text-center">
+            Already have an account?{" "}
+            <Link to="/login" className="text-yellow-400 hover:text-yellow-500 transition duration-200 font-semibold">
+              Login
+            </Link>
+          </p>
         </form>
-
-        <p className="mt-4 text-center">
-          Already have an account?{" "}
-          <a href="blank" className="text-pink-500 hover:text-pink-700">
-            Login
-          </a>
-        </p>
       </div>
     </main>
   );
